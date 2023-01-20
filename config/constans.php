f7d6ebcccc4eb5442346e21434d914130e1fa4ba
KoheiShibata <ko.bsk116@gmail.com>
2023-01-20 20:20:18 +0900

    #5 フィルター機能追加

--- config/constans.php
+++ config/constans.php
@@ -6,9 +6,7 @@ define("SORT", "filter.select.sort");
 
 define("YAHOO_API", "https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=dj00aiZpPXRJb0R5MUJzRVRSMyZzPWNvbnN1bWVyc2VjcmV0Jng9MDk-&genre_category_id=13457&image_size=300&results=100&in_stock=true");
 
-define("INSTAGRAM_API_URL", "https://graph.facebook.com/v15.0/");
-define("BUSINESS_ACCOUNT_ID", "17841457502307817");
-define("ACCESS_TOKEN", "&access_token=EAAJ4ck2CSpQBAAHnyRTMZAZB8kqyls4atSU368ASousZB8VcwtKDTgvRgIXclde0J6Pmav9qXFuNWAlMO9ZBibrJxoJV5igbq4KcmNiB2xaCp5pspDaKZABhwZBVBlgJ5eaRZCxIwZASU4MZCIbjs40Dw0kqICiU17RFDn0PNf1PN8ZBasQDihIVWlakSYNNlZCXCwZD")
+
 
 
 ?>
