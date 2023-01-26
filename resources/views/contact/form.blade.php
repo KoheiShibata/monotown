f3df7b2891dd22a1584b65aced7e2f8053d08cee
KoheiShibata <ko.bsk116@gmail.com>
2023-01-26 20:56:42 +0900

    #9 バリデーション実装

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -14,16 +14,26 @@
             @csrf
             <div class="form-group">
                 <label for="name" class="contact-form__label">お名前</label><br>
-                <input type="text" name="name" id="name" value="" class="form-control">
+                <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-control">
+                @if($errors->has("name"))
+                <p class="error-message">{{ $errors->first("name") }}</p>
+                @endif
             </div>
             <div class="form-group">
                 <label for="email" class="contact-form__label">メールアドレス</label><br>
-                <input type="email" name="email" id="email" value="" class="form-control">
+                <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control">
+                @if($errors->has("email"))
+                <p class="error-message">{{ $errors->first("email") }}</p>
+                @endif
             </div>
             <div class="form-group">
                 <label for="content" class="contact-form__label">お問い合わせ内容</label><br>
-                <textarea name="content" id="content" class="form-control" cols="30" rows="10"></textarea>
+                <textarea name="content" id="content" class="form-control" rows="10">{{ old('content') }}</textarea>
             </div>
+            @if($errors->has("content"))
+            <p class="error-message">{{ $errors->first("content") }}</p>
+            @endif
+
             <button type="submit">送信</button>
         </form>
     </div>
