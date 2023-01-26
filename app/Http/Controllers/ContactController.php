ec7e32c6cebfc4737dc42f5f4fde2f8a5295403a
KoheiShibata <ko.bsk116@gmail.com>
2023-01-26 17:31:12 +0900

    #9 view作成(仮)

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactSendmail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     //
+
+    public function contact() {
+        return view("/contact");
+    }
+
+
+    public function send(Request $request) {
+        Mail::to("kohei.techis@gmail.com")->send(new ContactSendmail());
+    }
+
+    
 }
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+@extends("layouts")
+
+@section("css")
+<link href="{{ asset('css/contact.css') }}" rel="stylesheet">
+@endsection
+
+@section("title", "お問い合わせ")
+
+@section("main")
+<section class="contact-form">
+    <h2 class="contact-form__title">お問い合わせ</h2>
+    <div class="contact-form-wrap">
+        <form action="">
+            <div class="form-group">
+                <label for="name" class="contact-form__label">お名前</label><br>
+                <input type="text" name="name" id="name" value="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email" class="contact-form__label">メールアドレス</label><br>
+                <input type="email" name="email" id="email" value="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="content" class="contact-form__label">お問い合わせ内容</label><br>
+                <textarea name="content" id="content" class="form-control" cols="30" rows="10"></textarea>
+            </div>
+            <button type="submit">送信</button>
+        </form>
+    </div>
+</section>
+@endsection
