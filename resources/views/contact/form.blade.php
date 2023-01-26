7eafbd6ad37ac3a84bacef914c27c79e8867e6d8
KoheiShibata <ko.bsk116@gmail.com>
2023-01-26 22:33:37 +0900

    #9 お問い合わせフォームレイアウト完成

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -8,33 +8,34 @@
 
 @section("main")
 <section class="contact-form">
-    <h2 class="contact-form__title">お問い合わせ</h2>
+    <h2 class="contact-form__title">お問い合わせフォーム</h2>
     <div class="contact-form-wrap">
         <form action="/contact" method="post">
             @csrf
             <div class="form-group">
-                <label for="name" class="contact-form__label">お名前</label><br>
+                <label for="name" class="contact-form__label required">お名前</label><br>
                 <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-control">
                 @if($errors->has("name"))
                 <p class="error-message">{{ $errors->first("name") }}</p>
                 @endif
             </div>
             <div class="form-group">
-                <label for="email" class="contact-form__label">メールアドレス</label><br>
+                <label for="email" class="contact-form__label required">メールアドレス</label><br>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control">
                 @if($errors->has("email"))
                 <p class="error-message">{{ $errors->first("email") }}</p>
                 @endif
             </div>
             <div class="form-group">
-                <label for="content" class="contact-form__label">お問い合わせ内容</label><br>
+                <label for="content" class="contact-form__label required">お問い合わせ内容</label><br>
                 <textarea name="content" id="content" class="form-control" rows="10">{{ old('content') }}</textarea>
+                @if($errors->has("content"))
+                <p class="error-message">{{ $errors->first("content") }}</p>
+                @endif
             </div>
-            @if($errors->has("content"))
-            <p class="error-message">{{ $errors->first("content") }}</p>
-            @endif
-
-            <button type="submit">送信</button>
+            <div class="btn-area">
+                <button type="submit" class="btn__submit">送信</button>
+            </div>
         </form>
     </div>
 </section>
